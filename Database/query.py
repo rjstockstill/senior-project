0ec86a654bd1bbd550d7483fe269c7952a36b128
Jude <rjstockstill@gmail.com>
2019-05-09 16:31:08 -0500

    Cleaning house

--- Database/query.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-
-with open('data.json') as json_file:
-  with open('insert.sql', 'w', encoding='utf-8') as output_file:
-    data = json.load(json_file)
-    for p in data['item']:
-      output_file.write('INSERT INTO mls VALUES(\'' + p['manufacturer'] + '\', \'' + p['generic_name'] + '\', \'' + p['brand_name'] + '\', \'' + p['route'] + '\', \'' + p['dosage'] + '\', \'' + p['adverse_reactions'] + '\', \'' + p['mechanism_of_action'] + '\', \'' + p['warnings'] + '\');\n')
